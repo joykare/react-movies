85f5d68bbf2b44a14185de361081b896b0d056ac
joykare <joy.warugu@andela.com>
2017-04-22 18:16:39 +0300

    Movie array onto container

--- client/components/CreateMovie.jsx
+++ client/components/CreateMovie.jsx
@@ -8,9 +8,9 @@ const CreateMovie = (props) => {
         <p>App to list movies</p>
       </div>
       <div className="input-group col-lg-4 offset-lg-4">
-        <input type="text" onBlur={props.handleBlur} placeholder="enter movie name" />
+        <input type="text" onBlur={props.addMovie} placeholder="enter movie name" />
         <div className="input-group-btn">
-          <button className="btn btn-primary">Add</button>
+          <button className="btn btn-primary" onClick={props.handleClick}>Add</button>
         </div>
       </div>
     </div>
--- client/containers/CreateMovieContainer.jsx
+++ client/containers/CreateMovieContainer.jsx
@@ -5,19 +5,33 @@ class CreateMovieContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      movie: "",
+      movies: []
     };
     this.handleBlur = this.handleBlur.bind(this);
-  }
-  
-  handleBlur = () => {
-    console.log("blur");
+    this.handleClick = this.handleClick.bind(this);
   }
 
+  handleBlur = (event) => {
+    event.preventDefault();
+    this.setState({
+      movie: event.target.value
+    })
+    console.log('movie', this.state.movie);
+  }
+
+  handleClick = (event) => {
+    const movieArray = this.state.movies;
+    movieArray.push(this.state.movie);
+    console.log(movieArray);
+  }
   render() {
     return (
       <div>
-        <CreateMovie handleBlur={this.handleBlur}/>
+        <CreateMovie addMovie={this.handleBlur}
+          handleClick={this.handleClick}
+          movies={this.state.movies}
+        />
       </div>
     );
   }
