065c9fc12fa42d8e29398a2ae253bfa92091081e
joykare <joy.warugu@andela.com>
2017-04-22 17:49:01 +0300

    format

--- client/components/CreateMovie.jsx
+++ client/components/CreateMovie.jsx
@@ -4,15 +4,15 @@ const CreateMovie = () => {
   return (
     <div>
       <div className="text-lg-center">
-      <h1>Movie App</h1>
-      <p>App to list movies</p>
-    </div>
-    <div className="input-group col-lg-4 offset-lg-4">
-      <input type="text" placeholder="enter movie name"/>
-      <div className="input-group-btn">
-        <button className="btn btn-primary">Add</button>
+        <h1>Movie App</h1>
+        <p>App to list movies</p>
       </div>
-    </div>
+      <div className="input-group col-lg-4 offset-lg-4">
+        <input type="text" placeholder="enter movie name" />
+        <div className="input-group-btn">
+          <button className="btn btn-primary">Add</button>
+        </div>
+      </div>
     </div>
   );
 };
